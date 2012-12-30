382363442bd1af820140848e59a4a24489645549
Guillaume Roguez <yomgui1@gmail.com>
2012-12-30 23:30:00 +0000

    removed MorphOS SDK workaround in bz2 module

--- Modules/bz2module.c
+++ Modules/bz2module.c
@@ -9,7 +9,6 @@ Copyright (c) 2002  Python Software Foundation; All Rights Reserved
 
 #include "Python.h"
 #include <stdio.h>
-#undef __MORPHOS_SHAREDLIBS /* FIXME: remove me when new fixed SDK is available */
 #include <bzlib.h>
 #include "structmember.h"
 
