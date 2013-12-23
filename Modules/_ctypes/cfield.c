c67b62ea4bd358763d3b3ccb09cfb0f9faf2aa13
Guillaume Roguez <yomgui1@gmail.com>
2013-12-23 11:44:32 -0500

    _ctype: remove unneeded dedicated MorphOS code

--- Modules/_ctypes/cfield.c
+++ Modules/_ctypes/cfield.c
@@ -905,23 +905,6 @@ q_set(void *ptr, PyObject *value, Py_ssize_t size)
     _RET(value);
 }
 
-#ifdef __MORPHOS__
-#include <hardware/byteswap.h>
-static PyObject *
-q_set_sw(void *ptr, PyObject *value, unsigned size)
-{
-	PY_LONG_LONG val;
-	PY_LONG_LONG field;
-	if (get_longlong(value, &val) < 0)
-		return NULL;
-	memcpy(&field, ptr, sizeof(field));
-	field = ((PY_LONG_LONG)SWAPLONG((ULONG)field)) << 32 | SWAPLONG((ULONG)(field>>32));
-	field = (PY_LONG_LONG)SET(field, val, size);
-	field = ((PY_LONG_LONG)SWAPLONG((ULONG)field)) << 32 | SWAPLONG((ULONG)(field>>32));
-	memcpy(ptr, &field, sizeof(field));
-	_RET(value);
-}
-#else
 static PyObject *
 q_set_sw(void *ptr, PyObject *value, Py_ssize_t size)
 {
@@ -936,7 +919,6 @@ q_set_sw(void *ptr, PyObject *value, Py_ssize_t size)
     memcpy(ptr, &field, sizeof(field));
     _RET(value);
 }
-#endif
 
 static PyObject *
 q_get(void *ptr, Py_ssize_t size)
@@ -970,23 +952,6 @@ Q_set(void *ptr, PyObject *value, Py_ssize_t size)
     _RET(value);
 }
 
-#ifdef __MORPHOS__
-#include <hardware/byteswap.h>
-static PyObject *
-Q_set_sw(void *ptr, PyObject *value, unsigned size)
-{
-	unsigned PY_LONG_LONG val;
-	unsigned PY_LONG_LONG field;
-	if (get_ulonglong(value, &val) < 0)
-		return NULL;
-	memcpy(&field, ptr, sizeof(field));
-	field = ((PY_LONG_LONG)SWAPLONG((ULONG)field)) << 32 | SWAPLONG((ULONG)(field>>32));
-	field = (unsigned PY_LONG_LONG)SET(field, val, size);
-	field = ((PY_LONG_LONG)SWAPLONG((ULONG)field)) << 32 | SWAPLONG((ULONG)(field>>32));
-	memcpy(ptr, &field, sizeof(field));
-	_RET(value);
-}
-#else
 static PyObject *
 Q_set_sw(void *ptr, PyObject *value, Py_ssize_t size)
 {
@@ -1001,7 +966,6 @@ Q_set_sw(void *ptr, PyObject *value, Py_ssize_t size)
     memcpy(ptr, &field, sizeof(field));
     _RET(value);
 }
-#endif
 
 static PyObject *
 Q_get(void *ptr, Py_ssize_t size)
